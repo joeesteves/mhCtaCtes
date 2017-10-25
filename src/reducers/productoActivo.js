b4b3bfdcd3c8680985f9922c3cc953ddbabbf802
ponyesteves <ajesteves@gmail.com>
2017-10-25 11:00:48 -0300

    Fix applydiscount y calculos de valores al bajar cantidad no bajaba el numero

--- src/reducers/productoActivo.js
+++ src/reducers/productoActivo.js
@@ -5,7 +5,7 @@ import { todayString, roundTwo } from '../helpers/misc'
 export default (state = {}, action) => {
   switch (action.type) {
     case productoActivoActions.fill:
-      return { ...action.producto, date: todayString, cantidad: 1, metodoPago: 'Mercado Pago', show: 'hola' }
+      return { productoOriginal: action.producto, ...action.producto, date: todayString, cantidad: 1, metodoPago: 'Mercado Pago', show: 'hola' }
     case productoActivoActions.update:
       return Maybe(action.updateObject.cantidad)
         .map(updateValues(state))
@@ -22,11 +22,11 @@ export default (state = {}, action) => {
 const updateValues = R.curry((producto, cantidad) => {
   if (!cantidad || cantidad < 1)
     return producto
-  return {
-    ...['costo', 'margen', 'costoEnvio', 'iva', 'precio', 'ingreso', 'comision', 'descuento']
-      .reduce((p, c) => ({ ...p, [c]: roundTwo((p[c] * cantidad)) }), { ...producto }),
-    cantidad
-  }
+  return applyDiscount({
+    ...['costo', 'margen', 'costoEnvio', 'iva', 'precio', 'ingreso', 'comision']
+      .reduce((p, c) => ({ ...p, [c]: roundTwo((p.productoOriginal[c] * cantidad)) }), { ...producto }),
+    cantidad, descuento: 0, applyDescuento:0
+  }, producto.descuento)
 })
 
 const applyDiscount = (state, descuento) => {
